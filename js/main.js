fe75b45a192b79026570a510b46ae8c6f7753f47
bordia98 <bhavyabordia@gmail.com>
2018-09-30 12:28:48 +0530

    start timer corrected and end timer added

--- js/main.js
+++ js/main.js
@@ -249,7 +249,26 @@ $(document).ready(function(){
         // If the count down is over, write some text 
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("timer").innerHTML = "Live Now !!";
+            countDownDate = new Date("Nov 1, 2018 00:00:00").getTime();
+            var now = new Date().getTime();
+        
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+            
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            // Output the result in an element with id="demo"
+            document.getElementById("timer").innerHTML ="<div class='start-in'>ends in:</div>"+ days + "<span>days  </span>: " + hours + "<span>hour</span>: "
+            + minutes + "<span>mins  </span>: " + seconds + "<span>secs  </span>";
+      
+            if(distance<0){
+              clearInterval(x);
+              document.getElementById("timer").innerHTML = "Event Ended";
+              }
         }
     }, 1000);
 
